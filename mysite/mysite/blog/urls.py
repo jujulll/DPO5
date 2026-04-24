240cd66afa0d68aab8eb7979051975cafbc66fd6
Julia Ivshina <ivshinayv@gmail.com>
2026-04-24 22:32:35 +0500

    ControlWork

--- mysite/mysite/blog/urls.py
+++ mysite/mysite/blog/urls.py
@@ -7,7 +7,10 @@ router = DefaultRouter()
 router.register(r'posts', views.PostViewSet, basename='post')
 
 urlpatterns = [
-    #path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='home'),
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('api/v1/', include(router.urls))
 ]
--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -12,11 +12,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 
+MESSAGE_TAGS = { 
+    messages.DEBUG: 'alert-secondary', 
+    messages.INFO: 'alert-info', 
+    messages.SUCCESS: 'alert-success', 
+    messages.WARNING: 'alert-warning', 
+    messages.ERROR: 'alert-danger',
+    } 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
